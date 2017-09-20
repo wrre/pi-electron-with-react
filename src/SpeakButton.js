c6d21fb4f65e264ab7ca46a37811b575746bb9b9
Peggy Liu <fnoihtlanl@gmail.com>
2017-09-20 09:28:53 +0800

    Show STT result after end of reveice audio

---
+++ src/SpeakButton.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import talk from './api';
+import config from './ignore/config';
+import BingSpeech from './lib/BingSpeech';
+import './SpeakButton.css';
+
+const client = new BingSpeech.RecognitionClient(config.bingSppech.key);
+const recording = false;
+let result = '';
+
+class SpeakButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: '',
+      imgName: 'mic_black.svg'
+    };
+  }
+
+  render() {
+    const img = require(`./image/${this.state.imgName}`);
+    return (
+      <div className="Speak">
+        <img src={img} width="70" height="100" onClick={this.handleClick.bind(this)}/>
+        <h2>{this.state.result}</h2>
+      </div>
+    );
+  }
+
+handleClick() {
+    if (!recording) {
+      recording = true;
+      result = '';
+      this.setState({imgName: 'mic_gray.svg'});
+
+      client.startMicAndContinuousRecognition();
+      client.onFinalResponseReceived = function (response) {
+        result = result+ response;
+      }
+    } else {
+      recording = false;
+
+      client.endMicAndContinuousRecognition();
+      result = result.replace(/\./g, ' ');
+
+      this.setState({result, imgName: 'mic_black.svg'});
+    }
+  }
+}
+
+export default SpeakButton;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SpeakButton from './SpeakButton.js';
+import './index.css';
+
+ReactDOM.render(
+  <SpeakButton />,
+  document.getElementById('root')
+);
